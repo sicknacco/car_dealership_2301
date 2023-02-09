bcd56b6fb19d936525f13e8910df4d6c7927f5b1
Nick Sacco <nicksacco10@gmail.com>
2023-02-08 17:18:30 -0800

    Fix: remove comma that ruined everything

--- lib/car.rb
+++ lib/car.rb
@@ -1,9 +1,9 @@
 class Car
-  attr_reader :name, :monthly_payment, :loan_length, :make, :model, :color,
-  def initialize(name, monthly_payment, loan_length)
-    @name = name
-    @make = name.split[0]
-    @model = name.split[1]
+  attr_reader :make_model, :make, :model, :monthly_payment, :loan_length, :color
+  def initialize(make_model, monthly_payment, loan_length)
+    @make_model = make_model
+    @make = make_model.split[0]
+    @model = make_model.split[1]
     @monthly_payment = monthly_payment
     @loan_length = loan_length
     @color = nil
