17c0d8cadd1ba4418b0de592741aec74b116f2a2
Nick Sacco <nicksacco10@gmail.com>
2023-02-08 17:20:23 -0800

    Fix: Broken tests. added missing step

--- spec/dealership_spec.rb
+++ spec/dealership_spec.rb
@@ -3,25 +3,27 @@ require './lib/car'
 require './lib/dealership'
 
 RSpec.describe Dealership do
-  before(:each) do
-    dealership = Dealership.new("Acme Auto", "123 Main Street")
-  end
 
   it 'exists' do
+    dealership = Dealership.new("Acme Auto", "123 Main Street")
+
     expect(dealership).to be_instance_of(Dealership)
   end
 
   it 'starts with an empty lot' do
+    dealership = Dealership.new("Acme Auto", "123 Main Street")
+
     expect(dealership.inventory).to eq([])
   end
 
   it 'can count inventory' do
+    dealership = Dealership.new("Acme Auto", "123 Main Street")
+
     expect(dealership.inventory_count).to eq(0)
   end
 
   it 'can add inventory' do
-    expect(dealership.inventory_count).to eq(0)
-    require 'pry'; binding.pry
+    dealership = Dealership.new("Acme Auto", "123 Main Street")
     car_1 = Car.new("Ford Mustang", 1500, 36)
     car_2 = Car.new("Toyota Prius", 1000, 48)
     dealership.add_car(car_1)
